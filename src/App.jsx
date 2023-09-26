e391e3b726af7ecf4046a8beb3cedd1df8a78fa7
zerocordeiro <juseh.r@gmail.com>
2023-09-26 10:51:04 +0100

    Buttons are changing their own state, which can be seen byt looking at their classes, that change from blue to red each time they are clicked on. But that's not translating to an immediate change in their color, even though MakeArea is called every time the buttons status are changed.

--- src/App.jsx
+++ src/App.jsx
@@ -3,46 +3,47 @@ import { useState } from 'react'
 
 import './App.css'
 
-
 var nomedosbotoes = 1;
+
 
 function App() {
 
+  const [tabuleiro, setTabuleiro] = useState([['a0','a1'],['b0','b1']]);
 
-function ButtonTile(){
-  return(
-    <button key={uniqueElement.index} className={uniqueElement.index}>{uniqueElement}</button>
-  );
-}
 
 function changeButton(posx,posy){ //activated by clicking on the buttons of 'tabuleiro' play area
   console.log('function changeButton (posx, posy) = ' + posx + ' , ' + posy);
   let tabuleiroalt=tabuleiro;
-  return(console.log('nada')
-  );
+  tabuleiroalt[posx][posy].clicked = !tabuleiroalt[posx][posy].clicked;
+  setTabuleiro[tabuleiroalt];
+  console.log('tabuleiro[pos].clicked: ' + tabuleiro[posx][posy]?'true':'false')   
+  return(     
+    MakeArea(tabuleiro)
+    );
 }
+
+
 
 function ButtonOrBreak(uniqueElement){ //returns the html elements to 'MakeArea' in order to correctly build the play area, by sending buttons to their respective rows and then a BreakRow when ending the lane.
   console.log('running ButtonOrBreak');
   if (uniqueElement == 'brbr'){
-    console.log('BR');
+    // console.log('BR');
     return(<br/>);
   } else{
     let posx=uniqueElement.posx;
-    console.log(uniqueElement.posx);
     let posy=uniqueElement.posy;
-    let butcolor=uniqueElement.value?'redbutton':'bluebutton';
-    // console.log('Not BR. uniqueElement: '+ uniqueElement);
+    let butcolor=uniqueElement.clicked?'redbutton':'bluebutton';
     return(
-      <button className='butcolor' onClick={()=>changeButton(posx,posy)}>{nomedosbotoes}</button> //TODO: get data from the element passed down from 'MakeArea' and use it as parameters for a function that will be activated 'onClick' to change the state of 'tabuleiro'. 
-    ); //have already set a way to define the button color. Now need to make it change with the click of the button
+      <button className={butcolor} onClick={()=>changeButton(posx,posy)}>{nomedosbotoes}</button>// 
+    ); 
   }
   
 }
 
+
 function MakeArea(doublearray){ //gets 'tabuleiro' from main App
   //'doublearray' is a misleading name, because it's actually a multidimensional array. It was called double at the beginning because at first this code was tested with a [[a0,a1],[b0,b1]] array.
-  // console.log('In MakeArea -> doublearray: ' + doublearray + '. length:' + doublearray.length)
+  console.log('In MakeArea -> doublearray: ' + doublearray + '. length:' + doublearray.length)
   let allelements = []; //sets new empty array to use in for loop
   let controlobject = {}; //sets net object for parameter control (defining what matrix cell each button stands for)
   // console.log('allelements: '+ allelements + '.');
@@ -50,26 +51,18 @@ function MakeArea(doublearray){ //gets 'tabuleiro' from main App
     let singlearray=doublearray[i];
     // console.log('singlearray: '+singlearray +'. length: ' + singlearray.length);
     for(let j=0; j<singlearray.length;j++){
-      console.log('In MakeArea. posx:' + i + ', posy:' +j);
+      // console.log('In MakeArea. posx:' + i + ', posy:' +j);
       controlobject.posx = j;
       controlobject.posy = i;
-      controlobject.value = doublearray[j][i].value;
-      console.log('controlobject{posx:' + controlobject.posx+', posy:' + controlobject.posy+'}');
-      allelements.push({posx:controlobject.posx,posy:controlobject.posy,value:controlobject.value}); // creates an object that pulls the position of each item in the original 'tabuleiro' array. This data will be used in the buttons to create an 'onClick' function that will change the state of each tile in 'tabueiro'.
+      controlobject.clicked = doublearray[j][i].clicked;
+      // console.log('controlobject{posx:' + controlobject.posx+', posy:' + controlobject.posy+'}');
+      allelements.push({posx:controlobject.posx,posy:controlobject.posy,clicked:controlobject.clicked}); // creates an object that pulls the position of each item in the original 'tabuleiro' array. This data will be used in the buttons to create an 'onClick' function that will change the state of each tile in 'tabueiro'.
     }
-    // singlearray.map(
-    //   item=>(allelements.push(item))
-    // );
-    // console.log('after mapping: allelements: '+allelements);
     allelements.push('brbr');
-    // console.log('allelements pushed "brbr"')
-    // console.log('allelements after brbr:'+ allelements+ '. length: ' + allelements.length);
-
   }
   return(
     allelements.map( //obs: allelements is a linear array
       (element)=>(
-        // <button>{element}</button>
         ButtonOrBreak(element) //
       )
     )
@@ -77,7 +70,6 @@ function MakeArea(doublearray){ //gets 'tabuleiro' from main App
 }
 
 
-  const [tabuleiro, setTabuleiro] = useState([['a0','a1'],['b0','b1']]);
 
   function resizeTabuleiro(wd){
     console.log('------------------------------------------'
@@ -95,38 +87,13 @@ function MakeArea(doublearray){ //gets 'tabuleiro' from main App
         var objecttabuleiro = {
           posx:column,
           posy:line,
-          value:false,
+          clicked:false,
         };
-        // console.log('objecttabuleiro['+ objecttabuleiro.posy + ']['+objecttabuleiro.posx+']. objecttabuleiro: '+ objecttabuleiro,);
         newline.push({objecttabuleiro});
-        if (column==0){console.log('column ==0' + objecttabuleiro);}
       }
       counterresizetab++;
       newtabuleiro.push(newline);
-      // console.log('newtabuleiro after push(newline): ' +newtabuleiro);
     }
-    // console.log(' newtabuleiro.length = ' + newtabuleiro.length);
-    // setTabuleiro(['a','a','a','a','a'],['a','a','a','a','a'],['a','a','a','a','a']);
-    // setTabuleiro((tabuleiro)=> (tabuleiro.push(newtabuleiro)));
-    // setTabuleiro([]);
-    // setTabuleiro([['a0','a1'],['b0','b1'],['c0','c1']]);
-    // alert('tabuleiro pos troca: ' + tabuleiro);
-    //   let sizetab = 4;
-    //   let differenttabuleiro = [];
-    //   let newlinetabuleiro = [];
-    //   for(let i =0; i< sizetab; i++){
-    //     for(let j=0;j<sizetab;j++){
-    //       newlinetabuleiro.push('');
-    //     }
-    //     differenttabuleiro.push(newlinetabuleiro);
-    //     // alert(newline);
-    //   }
-    //   alert('differenttabuleiro: ' + differenttabuleiro)
-    //   alert('tabuleiro: '+tabuleiro+ ' <br/> agora vai trocar');
-      
-    //   setTabuleiro(differenttabuleiro);
-    //   alert('novo tabuleiro: '+ tabuleiro);
-
       setTabuleiro(newtabuleiro);
       nomedosbotoes++;
       console.log('newtabuleiro: ' + newtabuleiro + '----------------------------------- --------------------------------------------------- --------------------------------------------------- ---------------------------------------------------- ----------------------------------------------------------------------------------------------------------- -----------------------tabuleiro after setTabuleiro (l.111): '+ tabuleiro);
@@ -155,6 +122,18 @@ function MakeArea(doublearray){ //gets 'tabuleiro' from main App
   // setTabuleiro([['a0','a1','a2'],['b0','b1','b2'],['c0','c1','c2']]);
   // let tabuleiro = [['a0','a1'],['b0','b1']];
   
+  function changeButton(posx,posy){ //activated by clicking on the buttons of 'tabuleiro' play area
+    console.log('function changeButton (posx, posy) = ' + posx + ' , ' + posy);
+    let tabuleiroalt=tabuleiro;
+    tabuleiroalt[posx][posy].clicked = !tabuleiroalt[posx][posy].clicked;
+    setTabuleiro[tabuleiroalt];
+    console.log('tabuleiro[pos].clicked: ' + tabuleiro[posx][posy]?true:false)   
+    return(     
+      MakeArea(tabuleiro)
+      );
+  }
+
+
   return (
   <div itemID='divapp'>
     <h1 key="gamearea" className='ola'>olá, mundo</h1>
