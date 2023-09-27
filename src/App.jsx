ae019b91bf7b4c44e60ce3aa0679776f18457717
zerocordeiro <juseh.r@gmail.com>
2023-09-27 20:27:04 +0100

    Main mechanic works; Clicking counter works; Tested for size compatibility on various screen sizes and orientations; automatic creation of gameplay area works; automatic filling some tiles works. TO-DO: Endgame check and message; work on the form bug that makes it deselect at each number typed in

--- src/App.jsx
+++ src/App.jsx
@@ -7,54 +7,56 @@ import './App.css'
 
 
 function App() {
+
   const [contagem, setContagem] = useState(0);
   const [tabuleiro, setTabuleiro] = useState([]);
   const [gamesize, setGameSize] = useState();
 
-  function CheckButton(){
-    return(
-      <button id="ver_tabuleiro" class="ver-tabuleiro" key="testbut" onClick={()=>(alert(
-        '0:' +
-        tabuleiro[0][0].clicked+ ' , ' + 
-        tabuleiro[0][1].clicked+ ' , ' + 
-        tabuleiro[0][2].clicked+ ' , ' + 
-        // tabuleiro[0][3].clicked+ ' , ' + 
-        // tabuleiro[0][4].clicked+ ' , ' + 
-        '\r\n' +
-        '2: ' +
-        tabuleiro[2][0]['clicked']+ ' , ' + 
-        tabuleiro[2][1]['clicked']+ ' , ' + 
-        tabuleiro[2][2]['clicked']+ ' , ' + 
-        // tabuleiro[2][3]['clicked']+ ' , ' + 
-        // tabuleiro[2][4]['clicked']+ ' , ' +
-        '\r\n' 
-        // + '4: ' +
-        // tabuleiro[4][0]['clicked']+ ' , ' + 
-        // tabuleiro[4][1]['clicked']+ ' , ' + 
-        // tabuleiro[4][2]['clicked']+ ' , ' + 
-        // tabuleiro[4][3]['clicked']+ ' , ' + 
-        // tabuleiro[4][4]['clicked']+ ' , ' 
-        ))}>Ver <br/> valor nas <br/> linhas <br/> 0 e -1.</button>
-      /* used to check value changes in the last row of the game board. Just to make sure that it's not getting 'undefined' instead of T or F*/
-    );
-  }
 
   function ButtonOrBreak(uniqueElement){ //returns the html elements to 'MakeArea' in order to correctly build the play area, by sending buttons to their respective rows and then a BreakRow when ending the lane.
   console.log('running ButtonOrBreak');
 
-  function changeButton(posy,posx){ //activated by clicking on the buttons of 'tabuleiro' play area
-    console.log('function changeButton (posy, posx) = ' + posy + ' , ' + posx);
-    let tabuleiroalt=tabuleiro;
-    tabuleiroalt[posy][posx].clicked = !tabuleiroalt[posy][posx].clicked;
-    // setTabuleiro[''];
-    // console.log('zerou tabuleiro');
-    setContagem((n)=>n+1);
-    setTabuleiro[tabuleiroalt];
-    // console.log('tabuleiro['+posy+']['+posx+'].clicked: ' + tabuleiro[posy][posx]['clicked'])
-    // return(     
-    //   MakeArea()
-    //   );
-  }
+    function changeButton(posy,posx){ //this is the function that will be called upon each button clicked in the 'tabuleiro' play area
+      console.log('function changeButton (posy, posx) = ' + posy + ' , ' + posx);
+      let tabuleiroalt=tabuleiro;
+      tabuleiroalt[posy][posx].clicked = !tabuleiroalt[posy][posx].clicked;    
+
+      // Code below will make spaces adjacent to the one clicked on to also change
+      // To make checks simpler and the code easier to visualize let's set four variables for the 4 possiblwe adjacent squares.
+      let posybefore = posy-1;
+      let posyafter = posy+1;
+      let posxbefore = posx-1;
+      let posxafter = posx+1;
+      if(posybefore>=0){
+        tabuleiroalt[posybefore][posx].clicked = !tabuleiroalt[posybefore][posx].clicked;
+      }
+      if(posyafter<tabuleiro[0].length){
+        tabuleiroalt[posyafter][posx].clicked = !tabuleiroalt[posyafter][posx].clicked;
+      }
+      if(posxbefore>=0){
+        tabuleiroalt[posy][posxbefore].clicked = !tabuleiroalt[posy][posxbefore].clicked;
+      }
+      if(posxafter<tabuleiro[0].length){
+        tabuleiroalt[posy][posxafter].clicked = !tabuleiroalt[posy][posxafter].clicked;
+      }
+      // End of change to adjacent squares
+      setContagem((n)=>n+1); //this is what makes the screen update with each click
+      setTabuleiro[tabuleiroalt];
+
+      //Checking for victory condition
+      let victory=false;
+      
+      for(let rownumber=0;rownumber<(tabuleiroalt.length-1);rownumber++){
+        if(tabuleiroalt[rownumber]==tabuleiroalt[rownumber+1]){
+          victory=true;
+        }
+      }
+      if(victory==true){
+        alert('YOU WIN!');
+      }
+
+      // End of check for victory condition
+    } //end of changeButton
   
   if (uniqueElement == 'brbr'){
     // console.log('BR');
@@ -62,62 +64,64 @@ function App() {
   } else{
     let posx=uniqueElement.posx;
     let posy=uniqueElement.posy;
-    let butcolor=uniqueElement.clicked?'redbutton':'bluebutton';
+    let butcolor=uniqueElement.clicked?'bluebutton':'redbutton';
     let keystring=String(posx)+String(posy);
-    let width_for_flex=window.innerWidth/(tabuleiro.length*1.1);
+    let width_for_flex=0.9*((window.innerWidth)/(tabuleiro.length));
+    if(window.innerHeight<window.innerWidth){width_for_flex=window.innerHeight/(tabuleiro.length);}
+      width_for_flex*=0.9;
     return(
-      <button style={{border:'4px solid black',minWidth:width_for_flex,}} key={keystring} className={butcolor} onClick={()=>changeButton(posy,posx)}></button>// 
+      <button style={{width:width_for_flex, maxWidth:width_for_flex,}} key={keystring} className={butcolor} onClick={()=>changeButton(posy,posx)}></button>// 
     ); 
   }
   
 }
 
 
-function MakeArea(){ //gets 'tabuleiro' from main App
-  //'doublearray' is a misleading name, because it's actually a multidimensional array. It was called double at the beginning because at first this code was tested with a [[a0,a1],[b0,b1]] array.
+  function MakeArea(){ //gets 'tabuleiro' from main App
+    //'doublearray' is a misleading name, because it's actually a multidimensional array. It was called double at the beginning because at first this code was tested with a [[a0,a1],[b0,b1]] array.
 
-    let doublearray=tabuleiro;
-    console.log('Running MakeArea')
-    let allelements = []; //sets new empty array to use in for loop
-    let controlobject = {}; //sets net object for parameter control (defining what matrix cell each button stands for)
-    // console.log('allelements: '+ allelements + '.');
-    for(let i = 0; i<doublearray.length; i++){
-      let singlearray=doublearray[i];
-      // console.log('singlearray: '+singlearray +'. length: ' + singlearray.length);
-      for(let j=0; j<singlearray.length;j++){
-        // console.log('In MakeArea. posx:' + i + ', posy:' +j);
-        controlobject.posx = j;
-        controlobject.posy = i;
-        controlobject.clicked = doublearray[i][j].clicked;
+      let doublearray=tabuleiro;
+      console.log('Running MakeArea')
+      let allelements = []; //sets new empty array to use in for loop
+      let controlobject = {}; //sets net object for parameter control (defining what matrix cell each button stands for)
+      // console.log('allelements: '+ allelements + '.');
+      for(let i = 0; i<doublearray.length; i++){
+        let singlearray=doublearray[i];
+        // console.log('singlearray: '+singlearray +'. length: ' + singlearray.length);
+        for(let j=0; j<singlearray.length;j++){
+          // console.log('In MakeArea. posx:' + i + ', posy:' +j);
+          controlobject.posx = j;
+          controlobject.posy = i;
+          controlobject.clicked = doublearray[i][j].clicked;
 
-        allelements.push({posx:controlobject.posx,posy:controlobject.posy,clicked:controlobject.clicked}); 
-        // creates an object that pulls the position of each item in the original 'tabuleiro' array. This data will be used in the buttons to create an 'onClick' function that will change the state of each tile in 'tabueiro'.
+          allelements.push({posx:controlobject.posx,posy:controlobject.posy,clicked:controlobject.clicked}); 
+          // creates an object that pulls the position of each item in the original 'tabuleiro' array. This data will be used in the buttons to create an 'onClick' function that will change the state of each tile in 'tabueiro'.
+        }
+        allelements.push('brbr');
       }
-      allelements.push('brbr');
-    }
-    allelements.map((item,index)=>(console.log(index)));
-    return(
-      <div className='gameArea'>{
-        allelements.map( //obs: allelements is a linear array
-        (element)=>(
-          ButtonOrBreak(element) //
-        )
-      )
-      }
+      allelements.map((item,index)=>(console.log(index)));
+      let size_tabuleiro=(window.innerWidth);
+      if(window.innerHeight<window.innerWidth){size_tabuleiro=window.innerHeight;}
+      size_tabuleiro*=0.9;
+      return(
         
-      </div>
-      
-    );
+        <div className='gameArea' style={{width:size_tabuleiro,height:size_tabuleiro,}}>{
+          allelements.map( //obs: allelements is a linear array
+          (element)=>(
+            ButtonOrBreak(element) //
+          )
+        )
+        }
+          
+        </div>
+        
+      );
 
-}
+  }
 
 
 
   function resizeTabuleiro(wd){
-    console.log('------------------------------------------'
-    +'------------------------------'+
-    '----------------------------------' + 
-    'running resizeTabuleiro')
     if(wd==tabuleiro.length){
       console.log('sem alteração');
     }else{
@@ -132,26 +136,67 @@ function MakeArea(){ //gets 'tabuleiro' from main App
       newtabuleiro.push(newline);
     }
       setTabuleiro(newtabuleiro);
+      setContagem(0);
+
+      //trying to make automatic changes to the playing board
+      for(let step=0;step<neww*2;step++){
+        let posyc = Math.floor(Math.random()*(neww - 0 + 1)) + 0;
+        let posxc = Math.floor(Math.random()*(neww - 0 + 1)) + 0;
+      //basically had to repeat the content of changeButton function because couldn't call it here, for some reason. 
+      // did some small changes just to accomodate variables names I was using and to make it easier to spot differences while reviewing
+      //For now it's working, but TODO: optimize this to reduce code size. 
+        let tabuleiroalt=newtabuleiro;
+      tabuleiroalt[posyc][posxc].clicked = !tabuleiroalt[posyc][posxc].clicked;    
+      let posybefore = posyc-1;
+      let posyafter = posyc+1;
+      let posxbefore = posxc-1;
+      let posxafter = posxc+1;
+      if(posybefore>=0){
+        tabuleiroalt[posybefore][posxc].clicked = !tabuleiroalt[posybefore][posxc].clicked;
+      }
+      if(posyafter<tabuleiroalt[0].length){
+        tabuleiroalt[posyafter][posxc].clicked = !tabuleiroalt[posyafter][posxc].clicked;
+      }
+      if(posxbefore>=0){
+        tabuleiroalt[posyc][posxbefore].clicked = !tabuleiroalt[posyc][posxbefore].clicked;
+      }
+      if(posxafter<tabuleiroalt[0].length){
+        tabuleiroalt[posyc][posxafter].clicked = !tabuleiroalt[posyc][posxafter].clicked;
+      }//Here ends the code copied from changeButton
+
+      }
     }
   }
-  function setNewGameSize(form){
+
+
+  function newGameSize(form){
     form.preventDefault();
-    //prevents reloading the whole page from getting data from the form
-    if(!gamesize){
+    //prevents reloading the whole page when getting data from the form
+    if(!gamesize||gamesize<=0){
       return(document.getElementById('input-size').focus());
     }else{
       resizeTabuleiro(gamesize);
       document.getElementById('input-size').focus();
     }
   }
-  function CircleButton(){ // used during testing of this code.
+
+
+  function CircleButton(){ 
+    // used during testing of this code.
     function handleClick(msg='default'){
-        resizeTabuleiro(3);
+        let randomnumber = Math.floor(Math.random()*(10 - 3 + 1)) + 3;
+        // Math.floor(Math.random()*(max - min + 1)) + min;
+        resizeTabuleiro(randomnumber);
         console.log(msg);
     }
     return(
       <>
-      <form onSubmit={setNewGameSize}>
+        <button className='circlebutton' onClick={()=>handleClick('clicked')}>
+      {/* need to pass the function instead of calling it directly or else it triggers as page loads*/}
+        Quick setup
+      </button>
+      <div className='formcontainer'>
+      <form onSubmit={newGameSize}>
         <input 
         id='input-size'
         type='number'
@@ -161,10 +206,9 @@ function MakeArea(){ //gets 'tabuleiro' from main App
         />
         <button className='setgame' type='submit'>Set game</button>
       </form>
-      <button className='circlebutton' onClick={()=>handleClick('clicked')}>
-      {/* need to pass the function instead of calling it directly or else it triggers as page loads*/}
-        Play game
-      </button>
+      </div>
+      
+      
       </>
     );
   }
@@ -179,8 +223,9 @@ function MakeArea(){ //gets 'tabuleiro' from main App
 
   return (
   <div itemID='divapp'>
-    <h1 key="gamearea" className='ola'>Hello</h1>
-    <p>Número de jogadas: {contagem}</p>
+    <h1 key="titlegame" className='ola'>Turn all buttons blue</h1>
+    <p className='cctext'>Click count: </p> <p className="cccount">{contagem}</p>
+
     <MakeArea />
     {/* MakeArea builds the gameplay area from the original  "matrix" tabuleiro*/}
     <br/>
@@ -188,7 +233,7 @@ function MakeArea(){ //gets 'tabuleiro' from main App
     {/* CircleButton is just a normal button put here to test some things, like sending some console messages.
     It's currently beign used to try to edit the 'tabuleiro' array.
     Also used to check if the page is at least partically being build */}
-    {/* <CheckButton /> */}
+      {/* <CheckButton /> */}
       <br/>
       {/* {JSON.stringify(tabuleiro)} */}
       {/* THis JSON is just to see the changes in the 'tabuleiro' array values*/}
