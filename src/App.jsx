c22548dd976f90ee703bdfdf62440dee0fdce690
zerocordeiro <juseh.r@gmail.com>
2023-09-28 15:55:29 +0100

    game now says when you finished it; input field no longer loses focus with each number typed;algorithm for randomizing boards is more effective, generates more interesting boards (added some checks dealing with the situations where it clicks twice on the same tile, effectively garanteeing that all tiles are activated at random in the setup of a game). Needs an endgame screen and something to make it stop when finished (not being able to click on the tile anymore after ending the current game)...

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,22 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 
-
-
 function App() {
 
   const [contagem, setContagem] = useState(0);
   const [tabuleiro, setTabuleiro] = useState([]);
   const [gamesize, setGameSize] = useState();
+  // const [victory,setVictory] = useState(false);
+  let victory = false;
+  
 
-
+  
   function ButtonOrBreak(uniqueElement){ //returns the html elements to 'MakeArea' in order to correctly build the play area, by sending buttons to their respective rows and then a BreakRow when ending the lane.
-  console.log('running ButtonOrBreak');
-
+  // console.log('running ButtonOrBreak');
+  
     function changeButton(posy,posx){ //this is the function that will be called upon each button clicked in the 'tabuleiro' play area
-      console.log('function changeButton (posy, posx) = ' + posy + ' , ' + posx);
+
+      // console.log('function changeButton (posy, posx) = ' + posy + ' , ' + posx);
       let tabuleiroalt=tabuleiro;
       tabuleiroalt[posy][posx].clicked = !tabuleiroalt[posy][posx].clicked;    
 
@@ -43,17 +45,30 @@ function App() {
       setContagem((n)=>n+1); //this is what makes the screen update with each click
       setTabuleiro[tabuleiroalt];
 
-      //Checking for victory condition
-      let victory=false;
-      
-      for(let rownumber=0;rownumber<(tabuleiroalt.length-1);rownumber++){
-        if(tabuleiroalt[rownumber]==tabuleiroalt[rownumber+1]){
-          victory=true;
+      //Checking for victory condition      
+      //goes through each row checking if all items are clicked. If it find one that isn't, will stop the loop as soon as it end the check by changing the value of the counters to the end condition
+      for(let posyn=0;posyn<(tabuleiroalt.length);posyn++){
+        for(let posxn=0;posxn<(tabuleiroalt.length);posxn++){
+          if(tabuleiroalt[posyn][posxn].clicked==true){
+            // setVictory(true);
+            victory=true;
+          }else{
+            // setVictory(false);
+            victory=false;
+            posxn=tabuleiroalt.length;
+            posyn=tabuleiroalt.length;
+
+          }
         }
       }
       if(victory==true){
-        alert('YOU WIN!');
+        
+        console.log('Congratulations! You used '+ (contagem+1) + ' clicks to make the board clear :)');
+        alert('Congratulations! You used '+ (contagem+1) + ' moves to complete this puzzle!');
+        
+        // setTabuleiro(['victory']);
       }
+
 
       // End of check for victory condition
     } //end of changeButton
@@ -79,9 +94,20 @@ function App() {
 
   function MakeArea(){ //gets 'tabuleiro' from main App
     //'doublearray' is a misleading name, because it's actually a multidimensional array. It was called double at the beginning because at first this code was tested with a [[a0,a1],[b0,b1]] array.
+    
+    // if(victoryMSG==false){return(
+    //   <div>FIM</div>
+    // );
+        // const screencontent = document.window.getElementById('gameArea');
+        // screencontent.style.backgroundColor="red";
+    // }
+    let size_tabuleiro=(window.innerWidth);
+    if(window.innerHeight<window.innerWidth){size_tabuleiro=window.innerHeight;}
+    size_tabuleiro*=0.9;
+    console.log(victory);
 
       let doublearray=tabuleiro;
-      console.log('Running MakeArea')
+      // console.log('Running MakeArea')
       let allelements = []; //sets new empty array to use in for loop
       let controlobject = {}; //sets net object for parameter control (defining what matrix cell each button stands for)
       // console.log('allelements: '+ allelements + '.');
@@ -95,99 +121,133 @@ function App() {
           controlobject.clicked = doublearray[i][j].clicked;
 
           allelements.push({posx:controlobject.posx,posy:controlobject.posy,clicked:controlobject.clicked}); 
-          // creates an object that pulls the position of each item in the original 'tabuleiro' array. This data will be used in the buttons to create an 'onClick' function that will change the state of each tile in 'tabueiro'.
+          // creates objects that contain the position of each item in the original 'tabuleiro' array. This data will be used in the buttons to create an 'onClick' function that will change the state of each tile in 'tabuleiro'.
         }
         allelements.push('brbr');
       }
-      allelements.map((item,index)=>(console.log(index)));
-      let size_tabuleiro=(window.innerWidth);
-      if(window.innerHeight<window.innerWidth){size_tabuleiro=window.innerHeight;}
-      size_tabuleiro*=0.9;
-      return(
-        
-        <div className='gameArea' style={{width:size_tabuleiro,height:size_tabuleiro,}}>{
-          allelements.map( //obs: allelements is a linear array
-          (element)=>(
-            ButtonOrBreak(element) //
-          )
-        )
-        }
+      // allelements.map((item,index)=>(console.log(index)));
+      
+
+
+
+        return(
           
-        </div>
-        
-      );
+          <div className='gameArea' itemID='gameArea' style={{width:size_tabuleiro,height:size_tabuleiro,}}>{
+            allelements.map( //obs: allelements is a linear array
+            (element)=>(
+              ButtonOrBreak(element) //
+            )
+          )
+          }
+            
+          </div>
+          
+        );
+
+      
 
   }
 
 
 
   function resizeTabuleiro(wd){
-    if(wd==tabuleiro.length){
-      console.log('sem alteração');
-    }else{
-    let neww=wd;
-    let newh=neww;
-    let newtabuleiro=[];
-    for (let pos1=0;pos1<newh;pos1++){
-      let newline=[];
-      for (let pos2=0;pos2<neww;pos2++){
-        newline.push({posx:pos2,posy:pos1,clicked:false});
+    // if(wd==tabuleiro.length){
+    //   console.log('sem alteração');
+    // }else{
+      let neww=wd;
+      let newh=neww;
+      let newtabuleiro=[];
+      for (let pos1=0;pos1<newh;pos1++){
+        let newline=[];
+        for (let pos2=0;pos2<neww;pos2++){
+          newline.push({posx:pos2,posy:pos1,clicked:true});
+        }
+        newtabuleiro.push(newline);
       }
-      newtabuleiro.push(newline);
-    }
-      setTabuleiro(newtabuleiro);
-      setContagem(0);
+        setTabuleiro(newtabuleiro);
+        setContagem(0);
 
-      //trying to make automatic changes to the playing board
-      for(let step=0;step<neww*2;step++){
-        let posyc = Math.floor(Math.random()*(neww - 0 + 1)) + 0;
-        let posxc = Math.floor(Math.random()*(neww - 0 + 1)) + 0;
-      //basically had to repeat the content of changeButton function because couldn't call it here, for some reason. 
-      // did some small changes just to accomodate variables names I was using and to make it easier to spot differences while reviewing
-      //For now it's working, but TODO: optimize this to reduce code size. 
-        let tabuleiroalt=newtabuleiro;
-      tabuleiroalt[posyc][posxc].clicked = !tabuleiroalt[posyc][posxc].clicked;    
-      let posybefore = posyc-1;
-      let posyafter = posyc+1;
-      let posxbefore = posxc-1;
-      let posxafter = posxc+1;
-      if(posybefore>=0){
-        tabuleiroalt[posybefore][posxc].clicked = !tabuleiroalt[posybefore][posxc].clicked;
-      }
-      if(posyafter<tabuleiroalt[0].length){
-        tabuleiroalt[posyafter][posxc].clicked = !tabuleiroalt[posyafter][posxc].clicked;
-      }
-      if(posxbefore>=0){
-        tabuleiroalt[posyc][posxbefore].clicked = !tabuleiroalt[posyc][posxbefore].clicked;
-      }
-      if(posxafter<tabuleiroalt[0].length){
-        tabuleiroalt[posyc][posxafter].clicked = !tabuleiroalt[posyc][posxafter].clicked;
-      }//Here ends the code copied from changeButton
+        //code bellow ('auto setter') makes automatic changes to the playing board so that each new board starts with a new random setup and is sure to have a solution.
+        // If the boards were to be always changed from "all unchecked" to "all checked" not all board sizes would have a possible solution.
+        // The way it's done now, the board is created "solved" and then some buttons are clicked, depending on the board size (code below), so it's made sure that all boards have a solution, as they were created "backwards"
 
-      }
-    }
+        // AUTO SETTER
+        let checkifmarked=[''];
+        for(let step=0;step<neww*neww;step++){
+          // This will go through the board "clicking" squares at random to set a new challenge each time the board is reset. 
+          //The number of steps is the same as the number of pieces in the board.
+          let posyc = Math.floor(Math.random()*(neww - 1 + 1)) + 1 -1;
+          let posxc = Math.floor(Math.random()*(neww - 1 + 1)) + 1 -1;
+
+          //AUTO SETTER CHECK
+          // these lines are meant to prevent the 'auto setter' from just cancelling any 'click' it already made (when testing without this check a lot of boards just came in already cleared). 
+          if(checkifmarked.includes({posy:posyc,posx:posxc})){
+            step--;
+            checkifmarked.splice((checkifmarked.indexOf({posyc,posxc})),1);
+            // by removing the coordinates from the 'checkifmarked' we allow them to be clicked on again. This isn't as optimized but is quite  easier than preventing the coordinate from being clicked on again.
+            // This, along with the number of steps in this loop being equal to the number of available pieces in the board, makes so that ALL pieces will be 'clicked' on at random.
+            // UNTESTED, UNPROVED conjecture: I believe this does not garantee that the puzzles will always be challenging, because it's possible that this random algorithm comes close to a solution to itsef (although unlikely or impossible that it will actually solve itself)
+          }else{
+            checkifmarked.push({posy:posyc,posx:posxc});
+          } 
+          // END OF AUTO SETTER CHECK.
+          
+        //basically had to repeat the content of changeButton function because couldn't call it here, for some reason. 
+        // did some small changes just to accomodate variables names I was using and to make it easier to spot differences while reviewing
+        //For now it's working, but TODO: optimize this to reduce code size. 
+          let tabuleiroalt=newtabuleiro;
+        tabuleiroalt[posyc][posxc].clicked = !tabuleiroalt[posyc][posxc].clicked;    
+        let posybefore = posyc-1;
+        let posyafter = posyc+1;
+        let posxbefore = posxc-1;
+        let posxafter = posxc+1;
+        if(posybefore>=0){
+          tabuleiroalt[posybefore][posxc].clicked = !tabuleiroalt[posybefore][posxc].clicked;
+        }
+        if(posyafter<tabuleiroalt[0].length){
+          tabuleiroalt[posyafter][posxc].clicked = !tabuleiroalt[posyafter][posxc].clicked;
+        }
+        if(posxbefore>=0){
+          tabuleiroalt[posyc][posxbefore].clicked = !tabuleiroalt[posyc][posxbefore].clicked;
+        }
+        if(posxafter<tabuleiroalt[0].length){
+          tabuleiroalt[posyc][posxafter].clicked = !tabuleiroalt[posyc][posxafter].clicked;
+        }//Here ends the code copied from changeButton
+
+        }
+
+    // }
   }
 
 
   function newGameSize(form){
     form.preventDefault();
     //prevents reloading the whole page when getting data from the form
-    if(!gamesize||gamesize<=0){
-      return(document.getElementById('input-size').focus());
+    // if(!gamesize||gamesize<=0){
+    //   return(document.getElementById('input-size').focus());
+    // }else{
+    //   resizeTabuleiro(gamesize);
+    //   document.getElementById('input-size').focus();
+    // }
+    if(gamesize>15){setGameSize(15);
+    }else if(gamesize<2){setGameSize(2);
     }else{
       resizeTabuleiro(gamesize);
-      document.getElementById('input-size').focus();
     }
+    
+
+    
+    document.getElementById('input-size').focus();
   }
 
 
   function CircleButton(){ 
-    // used during testing of this code.
+    // name heralded from the beginning of this code
     function handleClick(msg='default'){
         let randomnumber = Math.floor(Math.random()*(10 - 3 + 1)) + 3;
         // Math.floor(Math.random()*(max - min + 1)) + min;
         resizeTabuleiro(randomnumber);
-        console.log(msg);
+        // console.log(msg);
     }
     return(
       <>
@@ -214,22 +274,25 @@ function App() {
   }
   
   
-  useEffect(
-    ()=>{
-      console.log('Effect');
-      MakeArea();
-    },[tabuleiro]
-  )
+  // useEffect(
+  //   ()=>{
+  //     console.log('Effect');
+  //     MakeArea();
+  //   },[tabuleiro]
+  // )
 
   return (
+    
   <div itemID='divapp'>
     <h1 key="titlegame" className='ola'>Turn all buttons blue</h1>
     <p className='cctext'>Click count: </p> <p className="cccount">{contagem}</p>
 
-    <MakeArea />
+    <MakeArea key='makearea' id='makearea' />
     {/* MakeArea builds the gameplay area from the original  "matrix" tabuleiro*/}
     <br/>
-    <CircleButton key="circbut"/>
+    {/* <CircleButton key="circbut"/> */}
+    {CircleButton() } 
+    {/* by calling CircleButton like this we prevent the input field bein out of focus every time because of the page loading */}
     {/* CircleButton is just a normal button put here to test some things, like sending some console messages.
     It's currently beign used to try to edit the 'tabuleiro' array.
     Also used to check if the page is at least partically being build */}
@@ -244,3 +307,11 @@ function App() {
 
 
 export default App
+
+
+/*IDEAS:
+- find out if there's an algorithm for solving this kind of puzzle that can be used to check for the least steps necessary to solve a specific configuration. Use this to set difficulty levels (pre-checking boards before they're rendered) and to show a goal for the player.
+- make a way to save the current state of the board to keep playing after.
+- make a way to save and export the current state of the board for sharing
+
+*/
