95aef71ee3da7233af3766d58d5fbbd0685fac20
zerocordeiro <juseh.r@gmail.com>
2023-10-02 12:20:47 +0100

    Some more simplifications for the code. Final working version so far. Now I have to find out how to split this code and keep it working.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,39 @@
 import React from 'react'
 import { useState } from 'react'
 
-// import './App.css'
-
-
-
 function App() {
-
   const [counting, setCounting] = useState(0);
   const [buttoncolor,setButtonColor] = useState('blue');
+  const [buttoncolorant,setButtonColorAnt] = useState(buttoncolor);
   
   function ButtonCreator(){ 
     function changeButton(){ 
       if(buttoncolor!='blue'){
         setButtonColor('blue');
       }else{setButtonColor('red');}
-      setCounting((n)=>n+1); 
     }
     return(
       <button style={{width:200,height:200,backgroundColor:buttoncolor}} onClick={()=>changeButton()}>BUTTON</button>// 
     ); 
   }
+  function checkChanges(){
+    if(buttoncolorant!=buttoncolor){
+      setCounting((n)=>n+1); 
+      setButtonColorAnt(buttoncolor);
+    }
+  }
 
   function MakeArea(){
     return(<div className='gameArea' itemID='gameArea/'>{ButtonCreator()} </div> );
-    }
+  }
 
   return (
   <div itemID='divapp'>
     <p className='cctext'>Click count: </p> <p className="cccount">{counting}</p>
     <MakeArea key='makearea' itemID='makearea' />
+    {checkChanges()}
   </div>
   );
 }
-
-
 
 export default App
