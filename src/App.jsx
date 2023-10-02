f1e7c2934fb77d437541dd8e443a9bd997a942bb
zerocordeiro <juseh.r@gmail.com>
2023-10-02 13:18:18 +0100

    Tried making a fucntion that gets activated onClick and coitains both the function to change the button color and to update the counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,28 @@ import { useState } from 'react'
 
 function App() {
   const [counting, setCounting] = useState(0);
-  const [buttoncolor,setButtonColor] = useState('blue');
-  const [buttoncolorant,setButtonColorAnt] = useState(buttoncolor);
-  
+  const [buttoncolor,setButtonColor] = useState('blue');  
+
+  function checkChanges(){
+    setCounting((n)=>n+1); 
+  }
+  function changeButton(){ 
+    if(buttoncolor!='blue'){
+      setButtonColor('blue');
+    }else{setButtonColor('red');}
+  }
+
+  function changeButtonExtra(){
+    checkChanges();
+    changeButton();
+  }
   function ButtonCreator(){ 
-    function changeButton(){ 
-      if(buttoncolor!='blue'){
-        setButtonColor('blue');
-      }else{setButtonColor('red');}
-    }
+
     return(
-      <button style={{width:200,height:200,backgroundColor:buttoncolor}} onClick={()=>changeButton()}>BUTTON</button>// 
+      <button style={{width:200,height:200,backgroundColor:buttoncolor}} onClick={()=>changeButtonExtra()}>BUTTON</button>// 
     ); 
   }
-  function checkChanges(){
-    if(buttoncolorant!=buttoncolor){
-      setCounting((n)=>n+1); 
-      setButtonColorAnt(buttoncolor);
-    }
-  }
+
 
   function MakeArea(){
     return(<div className='gameArea' itemID='gameArea/'>{ButtonCreator()} </div> );
@@ -31,7 +34,6 @@ function App() {
   <div itemID='divapp'>
     <p className='cctext'>Click count: </p> <p className="cccount">{counting}</p>
     <MakeArea key='makearea' itemID='makearea' />
-    {checkChanges()}
   </div>
   );
 }
